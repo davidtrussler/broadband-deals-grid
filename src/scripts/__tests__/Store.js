107f6c76dd23fc698c4ce15a889d86a66a99f60d
David Trussler <mail@davidtrussler.net>
2021-02-28 12:12:48 +0000

    Sets up Filter-Scenario-2

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -1,4 +1,5 @@
 import Store from "../Store";
+import ViewFilters from "../ViewFilters"; 
 import mockData from "../../../public/db.json";
 
 describe("filter", () => {
@@ -10,6 +11,32 @@ describe("filter", () => {
 
 		// Act
 		const result = sut.deals;
+
+		// Assert
+		expect(result).toEqual(mockData.deals);
+	});
+
+	// Scenario 2: WHEN filtering by broadband THEN show the 4 broadband only deals
+	it("should return the 4 broadband only deals when filtering by broadband", () => {
+		// Arrange
+		const sut = new Store();
+		sut.setDeals(mockData.deals);
+
+		// const viewFilters = new ViewFilters(sut);
+
+		// viewFilters.update(sut.state);
+
+		// const filter_broadband = sut.setProductFilter('broadband');
+
+		// console.log('viewFilters: ', viewFilters);
+		// console.log('productFilters: ', productFilters);
+		// console.log('filter_broadband: ', filter_broadband);
+
+		// Act
+		const result = sut.deals;
+		sut.setProductFilter('broadband');
+
+		console.log('sut: ', sut); 
 
 		// Assert
 		expect(result).toEqual(mockData.deals);

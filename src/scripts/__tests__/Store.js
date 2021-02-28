f7614ee3dd79b4b612c8df9bec59ef5048165b5f
David Trussler <mail@davidtrussler.net>
2021-02-28 11:14:18 +0000

    Sets up repo for implementation of filter logic   - Forks and clones existing repo   - Makes minor slightly pedantic markup changes   - Adds Filter Scenario 1     - WHEN no filters applied THEN show all 11 deals

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -2,15 +2,16 @@ import Store from "../Store";
 import mockData from "../../../public/db.json";
 
 describe("filter", () => {
-  it("should return all deals when no filters applied", () => {
-    // Arrange
-    const sut = new Store();
-    sut.setDeals(mockData.deals);
+	// Scenario 1: WHEN filtering by broadband THEN show the 4 broadband only deals
+	it("should return all deals when no filters applied", () => {
+		// Arrange
+		const sut = new Store();
+		sut.setDeals(mockData.deals);
 
-    // Act
-    const result = sut.deals;
+		// Act
+		const result = sut.deals;
 
-    // Assert
-    expect(result).toEqual(mockData.deals);
-  });
+		// Assert
+		expect(result).toEqual(mockData.deals);
+	});
 });
